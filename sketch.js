010e9f93841c0573a32c88d814ae92629cc5359f
chenaxuan47-ai <chenaxuan47@gmail.com>
2025-12-19 18:30:41 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -16,7 +16,7 @@ let classifier;
 let label = "listening";
 
 // Teachable Machine model URL:
-let soundModelURL = 'https://teachablemachine.withgoogle.com/models/a283XtkI18/model.json';
+let soundModelURL = 'https://chenaxuan47-ai.github.io/audio11232610/model.json';
 
 
 function preload() {
@@ -50,4 +50,5 @@ function gotResult(error, results) {
   // The results are in an array ordered by confidence.
   // console.log(results[0]);
   label = results[0].label;
+
 }
